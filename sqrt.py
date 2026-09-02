e918c5daa23f77abe30ffdba0eeacc98f0a0403a
Hari-Prabhat <h2006pk@gmail.com>
2026-09-02 22:10:42 +0530

    some changes

--- sqrt.py
+++ sqrt.py
@@ -1,18 +1,20 @@
 import streamlit as st
-st.title=("Square Root Calculator")
+st.title("Square Root Calculator")
 
 st.write("This is a simple square root calculator. Enter a number below to calculate its square root.")
 
-st.number_input("Enter a number:", min_value=0.0, step=0.1, key="number")
+
+st.markdown("Enter a non-negative number in the input field.")
+number=st.number_input("Enter a number:", min_value=0.0, step=0.1)
 
 if st.button("Calculate Square Root"):
-    number = st.session_state.number
     if(number<0):
         st.error("Error: The square root of a negative number is not defined in the set of real numbers.")
+        st.warning("Note: The square root of a negative number is not defined in the set of real numbers.")
     else:
         square_root = number ** 0.5
-        st.write(f"The square root of {number} is {square_root}.")
+        st.write(f"The square root of {number}   is   {square_root}.")
 
-st.write("Note: The square root of a negative number is not defined in the set of real numbers.")
 
+st.write("Made by Hari Prabhat")
 
